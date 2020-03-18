f2ad548ce250a0922a02bd203d32de3f24e974c6
悠悠 <cautionex@163.com>
2020-03-19 00:20:52 +0800

    initialization

---
+++ src/github/apjifengc/bingo/Bingo.java
@@ -0,0 +1,17 @@
+package github.apjifengc.bingo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bingo extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Bingooooooooo!");
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("SAMPLE TEXT");
+	}
+	
+}
