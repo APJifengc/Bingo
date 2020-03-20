b2cc63c766c37c912696a505eca2881a84a1cd26
悠悠 <cautionex@163.com>
2020-03-20 12:03:32 +0800

    新增游戏盘工具类，新增玩家胜利检测，修改部分代码

--- src/github/apjifengc/bingo/command/StartCommand.java
+++ src/github/apjifengc/bingo/command/StartCommand.java
@@ -10,26 +10,25 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import java.io.IOException;
 
 public class StartCommand {
-    void onStartCommand(CommandSender sender,Bingo plugin) {
-        if (sender.hasPermission("bingo.admin.start")) {
-            if (!plugin.hasBingoGame()) {
-                BingoGame game = new BingoGame();
-                try {
-                    game.generateTasks();
-                } catch (IOException | InvalidConfigurationException | BadTaskException e) {
-                    e.printStackTrace();
-                    sender.sendMessage("§cAn error has occurred while running the command.\n§c"+e.getMessage());
-                    return;
-                }
-                plugin.setCurrentGame(game);
-                Bukkit.broadcastMessage("§9§l-==============  §c§lBingo§9§l  ==============-\n" +
-                                        "  §aA Bingo game has started!\n" +
-                                        "  §eUse command §7§n/bingo join§e to join the game!\n\n");
-            } else {
-                sender.sendMessage("§cThere is already a game running!");
-            }
-        } else {
-            sender.sendMessage("§cYou don't have the permission for this command.");
-        }
-    }
+	void onStartCommand(CommandSender sender, Bingo plugin) {
+		if (sender.hasPermission("bingo.admin.start")) {
+			if (!plugin.hasBingoGame()) {
+				BingoGame game = new BingoGame();
+				try {
+					game.generateTasks();
+				} catch (IOException | InvalidConfigurationException | BadTaskException e) {
+					e.printStackTrace();
+					sender.sendMessage("§cUnable to start a game: " + e.getMessage());
+					return;
+				}
+				plugin.setCurrentGame(game);
+				Bukkit.broadcastMessage(
+						"§9§l-==============  §c§lBingo§9§l  ==============-\n  §aA Bingo game has started!\n  §eUse command §7§n/bingo join§e to join the game!");
+			} else {
+				sender.sendMessage("§cThere is already a game running!");
+			}
+		} else {
+			sender.sendMessage("§cYou don't have the permission for this command.");
+		}
+	}
 }
--- src/github/apjifengc/bingo/game/BingoPlayer.java
+++ src/github/apjifengc/bingo/game/BingoPlayer.java
@@ -2,6 +2,7 @@ package github.apjifengc.bingo.game;
 
 import org.bukkit.entity.Player;
 
+import github.apjifengc.bingo.util.BingoBroad;
 import lombok.Getter;
 
 /**
@@ -18,27 +19,106 @@ public class BingoPlayer {
 	@Getter
 	BingoGame game;
 
-	boolean[] finishedTasks = new boolean[25];
+	boolean[] taskStatus = new boolean[25];
 
 	public BingoPlayer(Player player, BingoGame game) {
 		this.player = player;
 		this.game = game;
 	}
 
+	/**
+	 * 根据任务索引检查该玩家是否已完成该任务。
+	 * 
+	 * @param index
+	 * @return 若玩家已完成该任务则返回 true，反之返回 false。
+	 */
 	public boolean hasFinished(int index) {
-		return finishedTasks[index];
+		return taskStatus[index];
 	}
 
+	/**
+	 * 根据任务检查该玩家是否已完成该任务。
+	 * 
+	 * @param task
+	 * @return 若玩家已完成该任务则返回 true，反之返回 false。
+	 */
 	public boolean hasFinished(BingoTask task) {
-		return finishedTasks[game.getTasks().indexOf(task)];
+		return taskStatus[game.getTasks().indexOf(task)];
 	}
 
+	/**
+	 * 使玩家完成一项任务。
+	 * 
+	 * @param index 要完成的任务的索引
+	 */
 	public void finishTask(int index) {
-		finishedTasks[index] = true;
+		taskStatus[index] = true;
 	}
-	
+
+	/**
+	 * 使玩家完成一项任务。
+	 * 
+	 * @param index 要完成的任务
+	 */
 	public void finishTask(BingoTask task) {
-		finishedTasks[game.getTasks().indexOf(task)] = true;
+		taskStatus[game.getTasks().indexOf(task)] = true;
+	}
+
+	/**
+	 * 检查玩家是否已完成 Bingo。
+	 * 
+	 * @return 若已完成 Bingo 则返回true，反之返回 false。
+	 */
+	public boolean checkVictory() {
+		if (taskStatus[0] && taskStatus[6] && taskStatus[12] && taskStatus[18] && taskStatus[24])
+			return true;
+		if (taskStatus[4] && taskStatus[8] && taskStatus[12] && taskStatus[16] && taskStatus[20])
+			return true;
+		for (int i = 0, j = 0; i < 5; i++, j += 5) {
+			if (taskStatus[i] && taskStatus[i + 5] && taskStatus[i + 10] && taskStatus[i + 15] && taskStatus[i + 20])
+				return true;
+			if (taskStatus[j] && taskStatus[j + 1] && taskStatus[j + 2] && taskStatus[j + 3] && taskStatus[j + 4]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 根据任务索引检查玩家是否已完成相关 Bingo。
+	 * 
+	 * @param index 任务索引
+	 * @return 若已完成 Bingo 则返回true，反之返回 false。
+	 */
+	public boolean checkVictory(int index) {
+		if (index % 6 == 0) {
+			if (taskStatus[0] && taskStatus[6] && taskStatus[12] && taskStatus[18] && taskStatus[24])
+				return true;
+		} else if (index % 4 == 0 && index != 0) {
+			if (taskStatus[4] && taskStatus[8] && taskStatus[12] && taskStatus[16] && taskStatus[20])
+				return true;
+		}
+		int row = BingoBroad.getX(index);
+		if (taskStatus[row] && taskStatus[row + 5] && taskStatus[row + 10] && taskStatus[row + 15]
+				&& taskStatus[row + 20])
+			return true;
+		int col = BingoBroad.getYFirst(BingoBroad.getY(index));
+		if (taskStatus[col] && taskStatus[col + 1] && taskStatus[col + 2] && taskStatus[col + 3] && taskStatus[col + 4])
+			return true;
+		return false;
+	}
+
+	/**
+	 * 根据任务检查玩家是否已完成相关 Bingo。
+	 * @param task 要检查的任务
+	 * @return 若已完成相关 Bingo 则返回true，反之返回 false。
+	 */
+	public boolean checkVictory(BingoTask task) {
+		int index = game.getTasks().indexOf(task);
+		if (index != -1) {
+			return checkVictory(index);
+		}
+		return false;
 	}
 
 }
