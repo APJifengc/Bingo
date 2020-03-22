3cfb1dc029de4fd4510265860c011722eba7f48a
APJifengc <1500533774@qq.com>
2020-03-22 19:45:01 +0800

    配置小更新

--- src/github/apjifengc/bingo/Bingo.java
+++ src/github/apjifengc/bingo/Bingo.java
@@ -2,11 +2,16 @@ package github.apjifengc.bingo;
 
 import github.apjifengc.bingo.command.OnCommand;
 import github.apjifengc.bingo.game.BingoGame;
-import github.apjifengc.bingo.listener.ClickInventoryListener;
+import github.apjifengc.bingo.listener.InventoryListener;
+import github.apjifengc.bingo.listener.OtherListener;
+import github.apjifengc.bingo.listener.TaskListener;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.*;
 
 public class Bingo extends JavaPlugin {
 
@@ -14,15 +19,19 @@ public class Bingo extends JavaPlugin {
 	@Getter
 	BingoGame currentGame;
 
+	List<Listener> listenerList = new ArrayList<Listener>();
+
 	@Override
 	public void onEnable() {
 		getLogger().info("Bingooooooooo!");
 		this.getCommand("bingo").setExecutor(new OnCommand(this));
 		saveResource("tasks.yml", false);
 		saveResource("messages.yml", false);
-		saveResource("config.yml", false);
+		saveDefaultConfig();
 		Configs.reloadConfig(this);
-		getServer().getPluginManager().registerEvents(new ClickInventoryListener(this),this);
+		listenerList.add(new InventoryListener(this));
+		listenerList.add(new OtherListener(this));
+		listenerList.add(new TaskListener(this));
 	}
 
 	@Override
